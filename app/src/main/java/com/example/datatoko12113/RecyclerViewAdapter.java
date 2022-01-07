77134fb5bab9753afc1b1e286645006b8021e26f
NindaAnggiPutrisia <111201912113@mhs.dinus.ac.id>
2022-01-07 21:06:40 +0700

    datatoko12113

---
+++ app/src/main/java/com/example/datatoko12113/RecyclerViewAdapter.java
@@ -0,0 +1,129 @@
+package com.example.datatoko12113;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    //Deklarasi Variable
+    private ArrayList<data_toko> listToko;
+    private Context context;
+
+    //Membuat Interfece
+    public interface dataListener{
+        void onDeleteData(data_toko data, int position);
+    }
+
+    //Deklarasi objek dari Interfece
+    dataListener listener;
+
+    //Membuat Konstruktor, untuk menerima input dari Database
+    public RecyclerViewAdapter(ArrayList<data_toko> listMahasiswa, Context context) {
+        this.listToko = listToko;
+        this.context = context;
+        listener = (MyListData)context;
+    }
+
+    //ViewHolder Digunakan Untuk Menyimpan Referensi Dari View-View
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView Nama, Jenis, Kondisi, Hrg_beli, Hrg_jual;
+        private LinearLayout ListItem;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            //Menginisialisasi View-View yang terpasang pada layout RecyclerView kita
+            Nama = itemView.findViewById(R.id.nama);
+            Jenis = itemView.findViewById(R.id.jenis);
+            Kondisi = itemView.findViewById(R.id.kondisi);
+            Hrg_beli = itemView.findViewById(R.id.hrg_beli);
+            Hrg_jual = itemView.findViewById(R.id.hrg_jual);
+            ListItem = itemView.findViewById(R.id.list_item);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Membuat View untuk Menyiapkan dan Memasang Layout yang Akan digunakan pada RecyclerView
+        View V = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_design, parent, false);
+        return new ViewHolder(V);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        //Mengambil Nilai/Value yenag terdapat pada RecyclerView berdasarkan Posisi Tertentu
+        final String Nama = listToko.get(position).getNama();
+        final String Jenis = listToko.get(position).getJenis();
+        final String Kondisi = listToko.get(position).getKondisi();
+        final String Hrg_beli = listToko.get(position).getHrg_beli();
+        final String Hrg_jual = listToko.get(position).getHrg_jual();
+
+        //Memasukan Nilai/Value kedalam View (TextView: Nama, Jenis, Kondisi, Hrg_jual, Hrg_beli)
+        holder.Nama.setText("Nama: "+Nama);
+        holder.Jenis.setText("Jenis: "+Jenis);
+        holder.Kondisi.setText("Kondisi: "+Kondisi);
+        holder.Hrg_beli.setText("Hrg_beli: "+Hrg_beli);
+        holder.Hrg_jual.setText("Hrg_jual: "+Hrg_jual);
+
+        //Menampilkan Menu Update dan Delete saat user melakukan long klik pada salah satu item
+        holder.ListItem.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(final View view) {
+                final String[] action = {"Update", "Delete"};
+                AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
+                alert.setItems(action,  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        switch (i){
+                            case 0:
+                                /*
+                                  Berpindah Activity pada halaman layout updateData
+                                  dan mengambil data pada listMahasiswa, berdasarkan posisinya
+                                  untuk dikirim pada activity updateData
+                                 */
+                                Bundle bundle = new Bundle();
+                                bundle.putString("dataNama", listToko.get(position).getNama());
+                                bundle.putString("dataJenis", listToko.get(position).getJenis());
+                                bundle.putString("dataKondisi", listToko.get(position).getKondisi());
+                                bundle.putString("dataHrg_beli", listToko.get(position).getHrg_beli());
+                                bundle.putString("dataHrg_jual", listToko.get(position).getHrg_jual());
+                                bundle.putString("getPrimaryKey", listToko.get(position).getKey());
+                                Intent intent = new Intent(view.getContext(), updateData.class);
+                                intent.putExtras(bundle);
+                                context.startActivity(intent);
+                                break;
+                            case 1:
+                                //Menggunakan interface untuk mengirim data mahasiswa, yang akan dihapus
+                                listener.onDeleteData(listToko.get(position), position);
+                                break;
+                        }
+                    }
+                });
+                alert.create();
+                alert.show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        //Menghitung Ukuran/Jumlah Data Yang Akan Ditampilkan Pada RecyclerView
+        return listToko.size();
+    }
+
+}
---
+++ app/src/main/java/com/example/datatoko12113/updateData.java
@@ -0,0 +1,111 @@
+package com.example.datatoko12113;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+class updateData extends AppCompatActivity {
+
+    //Deklarasi Variable
+    private EditText namaBaru, jenisBaru, kondisiBaru, hrgbeliBaru, hrgjualBaru;
+    private Button update;
+    private DatabaseReference database;
+    private FirebaseAuth auth;
+    private String cekNama, cekJenis, cekKondisi, cekHrgbeli, cekHrgjual;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_data);
+        getSupportActionBar().setTitle("Update Data");
+        namaBaru = findViewById(R.id.new_nama);
+        jenisBaru = findViewById(R.id.new_jenis);
+        kondisiBaru = findViewById(R.id.new_kondisi);
+        hrgbeliBaru = findViewById(R.id.new_hrgbeli);
+        hrgjualBaru = findViewById(R.id.new_hrgjual);
+        update = findViewById(R.id.update);
+
+        //Mendapatkan Instance autentikasi dan Referensi dari Database
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
+        getData();
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Mendapatkan Data Mahasiswa yang akan dicek
+                cekNama = namaBaru.getText().toString();
+                cekJenis = jenisBaru.getText().toString();
+                cekKondisi = kondisiBaru.getText().toString();
+                cekHrgbeli = hrgbeliBaru.getText().toString();
+                cekHrgjual = hrgjualBaru.getText().toString();
+
+                //Mengecek agar tidak ada data yang kosong, saat proses update
+                if(isEmpty(cekNama) || isEmpty(cekJenis) || isEmpty(cekKondisi) || isEmpty(cekHrgbeli) || isEmpty(cekHrgjual)){
+                    Toast.makeText(updateData.this, "Data tidak boleh ada yang kosong", Toast.LENGTH_SHORT).show();
+                }else {
+                    //Menjalankan proses update data
+                    data_toko setToko = new data_toko();
+                    setToko.setNama(namaBaru.getText().toString());
+                    setToko.setJenis(jenisBaru.getText().toString());
+                    setToko.setKondisi(kondisiBaru.getText().toString());
+                    setToko.setHrg_beli(hrgbeliBaru.getText().toString());
+                    setToko.setHrg_jual(hrgjualBaru.getText().toString());
+                    updateToko(setToko);
+                }
+            }
+        });
+    }
+
+    // Mengecek apakah ada data yang kosong, sebelum diupdate
+    private boolean isEmpty(String s){
+        return TextUtils.isEmpty(s);
+    }
+
+    //Menampilkan data yang akan di update
+    private void getData(){
+        final String getNama = getIntent().getExtras().getString("dataNama");
+        final String getJenis = getIntent().getExtras().getString("dataJenis");
+        final String getKondisi = getIntent().getExtras().getString("dataKondisi");
+        final String getHrgbeli = getIntent().getExtras().getString("dataHrgbeli");
+        final String getHrgjual = getIntent().getExtras().getString("dataHrgjual");
+        namaBaru.setText(getNama);
+        jenisBaru.setText(getJenis);
+        kondisiBaru.setText(getKondisi);
+        hrgbeliBaru.setText(getHrgbeli);
+        hrgjualBaru.setText(getHrgjual);
+    }
+
+    //Proses Update data yang sudah ditentukan
+    private void updateToko(data_toko toko){
+        String userID = auth.getUid();
+        String getKey = getIntent().getExtras().getString("getPrimaryKey");
+        database.child("Admin")
+                .child(userID)
+                .child("Barang")
+                .child(getKey)
+                .setValue(toko)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        namaBaru.setText("");
+                        jenisBaru.setText("");
+                        kondisiBaru.setText("");
+                        hrgbeliBaru.setText("");
+                        hrgjualBaru.setText("");
+                        Toast.makeText(updateData.this, "Data Berhasil diubah", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+    }
+}
